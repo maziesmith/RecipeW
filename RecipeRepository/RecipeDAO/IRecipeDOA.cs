afd50baf379986e2c1c20393cfda8aa5e6033274
tommi.korteikko <tommi.korteikko@hotmail.com>
2019-03-03 22:00:17 +0200

    Commit after merge

---
+++ RecipeRepository/RecipeDAO/IRecipeDOA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeRepository
+{
+    public interface IRecipeDOA<T>
+    {
+        T Add(T obj);
+        T getByName(String Name);
+        T getById(double Id);
+        List<T> GetListOf(T obj);
+        int Remove(T obj);
+
+    }
+}
--- RecipeRepository/RecipeDAO/IncredientDOA.cs
+++ RecipeRepository/RecipeDAO/IncredientDOA.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using RecipeDomain;
 
 namespace RecipeRepository.RecipeDAO
 {
-    class IncredientDOA
+    public class IncredientDOA : IRecipeDOA<Incredient>
     {
+        public Incredient Add(Incredient obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Incredient getById(double Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Incredient getByName(string Name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Incredient> GetListOf(Incredient obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Remove(Incredient obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
