c09aff8536d76660090d1592af43fad72639c288
tommi.korteikko <tommi.korteikko@hotmail.com>
2019-03-03 16:37:56 +0200

    structure created

---
+++ RecipeApplication/RecipeControl/IncredientControl.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeApplication.RecipeControl
+{
+    class IncredientControl
+    {
+    }
+}
--- RecipeDomain/Incredient.cs
+++ RecipeDomain/Incredient.cs
@@ -2,7 +2,7 @@
 
 namespace RecipeDomain
 {
-    public class Class1
+    public class Incredient
     {
     }
 }
